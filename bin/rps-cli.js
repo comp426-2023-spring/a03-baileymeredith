231e7b119ef60e6b4e910fcebd3276ce688d9106
Bailey DeSouza <bdesouza@email.unc.edu>
2023-03-01 23:44:18 -0500

    added help and rule log blocks, may change later.

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -1,2 +1,32 @@
 #!/usr/bin/env node
 import { rps } from 'node-rpsls'
+import minimist from 'minimist'
+
+const args = minimist(process.argv.slice(2));
+
+// reformat the below? 
+if (args.h) {
+    // logs the help message
+    console.log('Usage: node-rps [SHOT]\n',
+    'Play Rock Paper Scissors (RPS)\n',
+    '\n',
+    '  -h, --help      display this help message and exit\n',
+    '  -r, --rules     display the rules and exit\n',
+    '\n',
+    'Examples:\n',
+    '  node-rps        Return JSON with single player RPS result.\n',
+    '                  e.g. {"player":"rock"}\n',
+    '  node-rps rock   Return JSON with results for RPS played against a simulated opponent.\n',
+    '                  e.g {"player":"rock","opponent":"scissors","result":"win"}')
+    process.exit(0)
+}
+
+if (args.r) {
+    // logs the rules
+    console.log('Rules for Rock Paper Scissors:\n',
+    '\n',
+    '  - Scissors CUTS Paper\n',
+    '  - Paper COVERS Rock\n',
+    '  - Rock CRUSHES Scissors')
+    process.exit(0)
+}
--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -1,2 +1,36 @@
 #!/usr/bin/env node
 import { rpsls } from 'node-rpsls'
+import minimist from minimist
+
+const args = minimist(process.argv.slice(2));
+
+if (args.h) {
+    console.log('Usage: node-rpsls [SHOT]\n',
+    'Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!\n',
+    '\n',
+    '  -h, --help        display this help message and exit\n',
+    '  -r, --rules       display the rules and exit\n',
+    '\n',
+    'Examples:\n',
+    '  node-rpsls        Return JSON with single player RPSLS result.\n',
+    '                    e.g. {"player":"rock"}\n',
+    '  node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.\n',
+    '                    e.g {"player":"rock","opponent":"Spock","result":"lose"}')
+    process.exit(0)
+}
+
+if (args.r) {
+    console.log('Rules for the Lizard-Spock Expansion of Rock Paper Scissors:\n',
+    '\n',
+    '  - Scissors CUTS Paper\n',
+    '  - Paper COVERS Rock\n',
+    '  - Rock SMOOSHES Lizard\n',
+    '  - Lizard POISONS Spock\n',
+    '  - Spock SMASHES Scissors\n',
+    '  - Scissors DECAPITATES Lizard\n',
+    '  - Lizard EATS Paper\n',
+    '  - Paper DISPROVES Spock\n',
+    '  - Spock VAPORIZES Rock\n',
+    '  - Rock CRUSHES Scissors')
+    process.exit(0)
+}
