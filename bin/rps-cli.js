f2534b5476f8b0c49f2c638525f315e2ceb1de55
Bailey DeSouza <bdesouza@email.unc.edu>
2023-03-02 22:47:47 -0500

    added comments.

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -5,18 +5,23 @@ import minimist from 'minimist'
 const args = minimist(process.argv.slice(2));
 
 if (args.h || args.help) {
+    // displays the help message if -h or -help is given in command prompt and exits application
     helpMessage();
     process.exit(0);
 }
 if (args.r || args.rules) {
+    // displays the rules if -r or -rules is given in command prompt and exits application
     ruleMessage();
     process.exit(0);
 }
+// move is assigned to first unlabeled arg after 'node-rps ' in command prompt
 const move = args._[0]
 try {
+    // tries to play rps and logs the result of game
     gameObject = rps(move)
     console.log(JSON.stringify(rps(gameObject)));
 } catch (e){
+    // if an error occurs and out-of-range argument is used, displays the rules and help message
     helpMessage()
     ruleMessage()
     process.exit(0)
@@ -24,6 +29,7 @@ try {
 
 
 function helpMessage() {
+    // logs the help message
     console.log('Usage: node-rps [SHOT]\nPlay Rock Paper Scissors (RPS)\n\n',
     '  -h, --help      display this help message and exit\n',
     '  -r, --rules     display the rules and exit\n\nExamples:\n',
@@ -34,6 +40,7 @@ function helpMessage() {
 }
 
 function ruleMessage() {
+    // logs the rules
     console.log('Rules for Rock Paper Scissors:\n\n',
     ' - Scissors CUTS Paper\n',
     ' - Paper COVERS Rock\n',
--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -5,24 +5,30 @@ import minimist from 'minimist';
 const args = minimist(process.argv.slice(2));
 
 if (args.h || args.help) {
+    // displays help message if -h or -help is given in command prompt and exits application
     helpMessage();
     process.exit(0);
 }
 if (args.r || args.rules) {
+    // displays rules if -r or -rules is given in command prompt and exits application
     ruleMessage();
     process.exit(0);
 }
+// move is assigned to first unlabeled arg after 'node-rpsls ' in command prompt
 const move = args._[0]
 try {
+    // tries to play rpsls game and logs the result
     gameObject = rps(move)
     console.log(JSON.stringify(rpsls(gameObject)))
 } catch (e) {
+    // displays help message and rules if an error occurs and out-of-range arg is used
     helpMessage()
     ruleMessage()
     process.exit(0)
 }
 
 function helpMessage() {
+    // logs the help message
     console.log('Usage: node-rpsls [SHOT]\nPlay the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!\n\n',
     '  -h, --help        display this help message and exit\n',
     '  -r, --rules       display the rules and exit\n\nExamples:\n',
@@ -33,6 +39,7 @@ function helpMessage() {
 }
 
 function ruleMessage() {
+    // logs the rules
     console.log('Rules for the Lizard-Spock Expansion of Rock Paper Scissors:\n\n',
     ' - Scissors CUTS Paper\n',
     ' - Paper COVERS Rock\n',
